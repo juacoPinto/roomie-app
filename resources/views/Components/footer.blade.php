e09c2e1fedc775d84c661eae94f73b51e46ffc7e
juacoPinto <juacopinto@gmail.com>
2024-10-01 23:02:47 -0300

    integracion de tailwind

---
+++ resources/views/Components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <h1>Seccion Para el Footer (Esto se removera, es solo para ubicar el lugar de este componente)</h1>
+    <p>Help</p>
+    <p>Team</p>
+    <p>Blog</p>
+    <p>Report</p>
+    <p>Terms</p>
+    <p>Privacy</p>
+</footer>
---
+++ resources/views/Components/nav-bar.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <a>Logo</a>
+    <div>
+        <button>
+            <svg></svg>
+            <span>Empieza tu Busqueda</span>
+        </button>
+    </div>
+    <a>Inicia sesion</a>
+</nav>
